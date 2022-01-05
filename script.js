186f773e5f330d2bd090453196ad015c72049d31
Victor <contatoanacletovictor@gmail.com>
2022-01-04 21:10:15 -0300

    LampPage

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+let btn = document.getElementById('btn');
+let luz = document.getElementById('luz');
+
+function toggleBtn() {
+    btn.classList.toggle('active');
+    luz.classList.toggle("on");
+};
